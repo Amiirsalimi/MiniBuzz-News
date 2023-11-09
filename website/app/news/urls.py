4100f08fb6239cd59a26d403094c649063e7d71f
Amiirsalimi <asalimi.94@gmail.com>
2023-11-09 12:50:26 +0500

    formated

--- website/app/news/urls.py
+++ website/app/news/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('news/<str:news_type>/', views.news_by_type, name='news_by_type'),
     path('user_news/', views.user_news, name='user_news'),
     path('create/', views.NewsEntryCreateView.as_view(), name='news-entry-create'),
-    path('news/<int:pk>/edit/', views.NewsEntryUpdateView.as_view(), name='news-entry-update'),
-    path('news/<int:pk>/delete/', views.NewsEntryDeleteView.as_view(), name='news-entry-delete'),
+    path('news/<int:pk>/edit/', views.NewsEntryUpdateView.as_view(),
+         name='news-entry-update'),
+    path('news/<int:pk>/delete/', views.NewsEntryDeleteView.as_view(),
+         name='news-entry-delete'),
 ]
